b8ff5ff4032143edfee9ad9ff8c55293fe2dce49
Bismit Wagle <bismitwagle@gmail.com>
2025-12-03 18:48:12 +0530

    Added Task 4

--- prototypesChallenges.js
+++ prototypesChallenges.js
@@ -52,6 +52,20 @@ Create a class Vehicle with properties make and model, and a method getDetails()
 Create a subclass Car that extends Vehicle and adds a method startEngine() that returns "Engine started".
 */
 
+/* 
+Method Overriding in Inheritance
+Extend the Vehicle class from the previous task to include a method move() that returns "The vehicle is moving".
+Then, override the move() method in the Car class to return "The car is driving".
+*/
+
+/* 
+Static Methods in Classes
+Add a static method isVehicle(obj) to the Vehicle class that checks if a given object is an instance of Vehicle. 
+The method should return true if the object is a Vehicle or a subclass of Vehicle, and false otherwise.
+*/
+
+
+
 class Vehicle{
     constructor(make,model){
         this.make=make
@@ -61,16 +75,28 @@ class Vehicle{
         return`Make:${this.make}, Model:${this.model}`
 
     }
+
+    move(){
+        return `The vehicle is moving`;
+    }
+    static isVehicle(obj){
+        return obj instanceof Vehicle;
+    }
 }
 
 class Car extends Vehicle{
     startEngine(){
         return `Engine Started`
     }
+    move(){
+        return `The car is driving`
+    }
 }
 
 const myCar= new Car("Volkswagan","Polo")
 console.log(myCar.getDetails());
 console.log(myCar.startEngine());
+
+
 
 
